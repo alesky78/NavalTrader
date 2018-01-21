abaa2acd807a6f6774fbbbfd6facc7ce6b63ae15
alesky78 <alessandro.dottavio@gmail.com>
2018-01-21 08:27:14 +0100

    add note for dialog

--- src/main/java/it/spaghettisource/navaltrader/ui/MainDesktopPane.java
+++ src/main/java/it/spaghettisource/navaltrader/ui/MainDesktopPane.java
@@ -3,14 +3,17 @@ package it.spaghettisource.navaltrader.ui;
 import javax.swing.JDesktopPane;
 import javax.swing.JOptionPane;
 
+//TODO verify where the messages are open if put logic to center them
 public class MainDesktopPane extends JDesktopPane {
 
 	
 	public void showInfoMessageDialog(String message) {
+		//TODO add the icon here
 		JOptionPane.showInternalMessageDialog(this, message,"info",JOptionPane.INFORMATION_MESSAGE,  null);
 	}
 
 	public void showErrorMessageDialog(String message) {
+		//TODO add the icon here		
 		JOptionPane.showInternalMessageDialog(this, message,"info",JOptionPane.ERROR_MESSAGE,  null);
 	}	
 	
