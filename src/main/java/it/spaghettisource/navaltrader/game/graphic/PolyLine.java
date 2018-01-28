fa31254c26a4e2878d4646dc0011a0bda6bae779
alesky78 <alessandro.dottavio@gmail.com>
2018-01-28 22:42:54 +0100

    add poligon rendering object

---
+++ src/main/java/it/spaghettisource/navaltrader/game/graphic/PolyLine.java
@@ -0,0 +1,37 @@
+package it.spaghettisource.navaltrader.game.graphic;
+
+import java.awt.Graphics;
+import java.util.*;
+
+
+/*
+ * The PolyLine class model a line made up of many points
+ */
+public class PolyLine {
+
+	private int points;
+	private int[] xPoints;
+	private int[] yPoints;  	
+
+	public PolyLine() {
+		points = 0;
+		xPoints = new int[0];
+		yPoints = new int[0];		
+	}
+
+	public void addPoint(int x, int y) {
+		//extend the array
+		points++;
+		xPoints = Arrays.copyOf(xPoints, points);
+		yPoints = Arrays.copyOf(yPoints, points);
+		
+		//add the new point
+		xPoints[points-1] = x;
+		yPoints[points-1] = x;		
+		
+	}
+
+	public void draw(Graphics g) { // draw itself
+		g.drawPolyline(xPoints,yPoints, points);
+	}
+}
