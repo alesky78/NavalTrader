6e019e113f0fb9419eff5fcc45f0a15ba0e8d2a8
D'OTTAVIO Alessandro <alessandro.dottavio.ext@proximus.com>
2018-02-20 11:42:26 +0100

    utility for coordinate conversion

---
+++ src/main/java/it/spaghettisource/navaltrader/draw/ScreeCoordinteUtil.java
@@ -0,0 +1,43 @@
+package it.spaghettisource.navaltrader.draw;
+
+import javax.swing.JComponent;
+
+import it.spaghettisource.navaltrader.geometry.Point;
+
+public class ScreeCoordinteUtil {
+
+	private ScreeCoordinteUtil(){
+	}
+
+	
+	/**
+	 * covert a point from real coordinate to the coordinate of a component
+	 * 
+	 * @param component where to draw over
+	 * @param realworldCoordinate
+	 * @param wolrdSize
+	 * @return
+	 */
+	public static Point convertFromRealWorldToScreenCoordinate(JComponent component, Point realworldCoordinate, int wolrdSize){
+		return new Point((int)( (double)realworldCoordinate.getX() * ((double)component.getWidth()  / (double)wolrdSize) ),
+						 (int)( (double)realworldCoordinate.getY() * ((double)component.getHeight() / (double)wolrdSize) ));
+	}
+
+	
+	/**
+	 * covert a point from real coordinate to the coordinate of a component
+	 * 
+	 * @param component
+	 * @param componentToDraw
+	 * @param realworldCoordinate
+	 * @param wolrdSize
+	 * @return
+	 */
+	public static void setLocationFromRealWorldToScreenCoordinate(JComponent component,JComponent componentToDraw, Point realworldCoordinate, int wolrdSize){
+		componentToDraw.setLocation((int)( (double)realworldCoordinate.getX() * ((double)component.getWidth()  / (double)wolrdSize) - (componentToDraw.getWidth() /2) ),
+									(int)( (double)realworldCoordinate.getY() * ((double)component.getHeight() / (double)wolrdSize) - (componentToDraw.getHeight()/2) ));
+
+	}
+	
+	
+}
